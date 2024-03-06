62c54de651e9df80eb798d8954cdb7f3baa1ab4c
Bhaksar <bhaskarmandal369@gmail.com>
2024-03-06 17:21:23 +0530

    delete button added

---
+++ url-shortener-frontend/src/components/modal.tsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+
+function ModalComponent() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleModal = () => {
+        setIsVisible(!isVisible);
+    };
+
+    const handleYesClick = () => {
+        console.log("Yes, I'm sure");
+        setIsVisible(false);
+    };
+
+    const handleNoClick = () => {
+        console.log("No, cancel");
+        setIsVisible(false);
+    };
+
+    return (
+        <>
+            <button
+                data-modal-target="popup-modal"
+                data-modal-toggle="popup-modal"
+                type="button"
+                onClick={toggleModal}
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="22px" height="22px">
+                    <path fill="#9ca3af" d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z" />
+                </svg>
+            </button>
+
+            {isVisible && (
+                <div
+                    id="popup-modal"
+                    tabIndex={-1}
+                    className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full bg-black bg-opacity-50"
+                >
+                    <div className="relative p-4 w-full max-w-md">
+                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <button
+                                type="button"
+                                className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                data-modal-hide="popup-modal"
+                                onClick={toggleModal}
+                            >
+                                <svg
+                                    className="w-3 h-3"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 14 14"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                    />
+                                </svg>
+                                <span className="sr-only">Close modal</span>
+                            </button>
+                            <div className="p-4 md:p-5 text-center">
+                                <svg
+                                    className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 20 20"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                                    />
+                                </svg>
+                                <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                                    Are you sure you want to delete this product?
+                                </h3>
+                                <button
+                                    data-modal-hide="popup-modal"
+                                    type="button"
+                                    className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+                                    onClick={handleYesClick}
+                                >
+                                    Yes, I'm sure
+                                </button>
+                                <button
+                                    data-modal-hide="popup-modal"
+                                    type="button"
+                                    className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                    onClick={handleNoClick}
+                                >
+                                    No, cancel
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
+
+export default ModalComponent;
--- url-shortener-frontend/src/components/tableCompo.tsx
+++ url-shortener-frontend/src/components/tableCompo.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import DataModel from "../models/datamodel";
 import '../styles/styles.css'
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import ModalComponent from "./modal.tsx";
 
 function TableComponent({ dataList }: { dataList: DataModel[] }) {
     const backendDomain = process.env.REACT_APP_BACKEND_DOMAIN
@@ -19,7 +22,7 @@ function TableComponent({ dataList }: { dataList: DataModel[] }) {
                             Clicked
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            
+
                         </th>
                     </tr>
                 </thead>
@@ -35,7 +38,7 @@ function TableComponent({ dataList }: { dataList: DataModel[] }) {
                         dataList.map((data, index) => (
                             <tr key={index} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                                 <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    <a href={"http://"+backendDomain+"/"+data.shorturl} target="_blank" className="font-medium  hover:underline"> {process.env.REACT_APP_BACKEND_DOMAIN}/{data.shorturl}</a>
+                                    <a href={"http://" + backendDomain + "/" + data.shorturl} target="_blank" className="font-medium  hover:underline"> {backendDomain}/{data.shorturl}</a>
                                 </td>
                                 <td className="px-6 py-4">
                                     <a href={data.url} target="_blank" className="font-medium hover:underline">{data.url}</a>
@@ -44,7 +47,15 @@ function TableComponent({ dataList }: { dataList: DataModel[] }) {
                                     {data.clicked}
                                 </td>
                                 <td className="px-6 py-4">
-                                    {data.clicked}
+                                    {/* <Popup trigger={<button >
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="22px" height="22px">
+                                            <path fill="#9ca3af" d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z" />
+                                        </svg>
+                                    </button>} position="center center">
+                                        <div className="popUpMain"> POPUP</div>
+                                    </Popup> */}
+                                    <ModalComponent />
+
                                 </td>
                             </tr>
                         ))
